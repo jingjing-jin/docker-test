ef447e5efdac028e1e7e4f1964025c14b15c5092
jingjing515 <jingm515@gmail.com>
2024-12-06 14:49:27 -0800

    storage volume mapping from the host to docker container

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ docker build -t docker_test .
 ```
 ### Run app
 ```sh
- docker run -it -p 8080:3001 docker_test
+docker run -it -p 8080:3001 docker_test
+```
+### Share stroage with bind mounts
+```sh
+docker run -it -v ./website:/public -p 8080:3001 docker_test
+# mapping local directory './website' from host system to the docker container directory'/public'      
 ```
 ### Test
 ```sh
--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 //middlewares
 app.use(express.json({ type: "application/json" }));
 
+//server website
+//create a virtual path prefix (where the path does not actually exist in the file system) for files that are served by the express.static function, specify a mount path for the directory
+app.use('/virtual/path', express.static('/public'))
+
 //routes
 app.get("/ping", ping);
 
